e750d4d04210e014a0304737bd7bc16a8e822346
showmaker2020 <116152651+showmaker2020@users.noreply.github.com>
2022-11-04 17:04:43 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,105 +1,94 @@
-// You can change global variables here:
-var radius = 240; // how big of the radius
-var autoRotate = true; // auto rotate or not
-var rotateSpeed = -60; // unit: seconds/360 degrees
-var imgWidth = 120; // width of images (unit: px)
-var imgHeight = 170; // height of images (unit: px)
+const canvans = document.getElementById('canvas1');
+const ctx = canvans.getContext('2d');
+canvans.width = window.innerWidth;
+canvans.height = window.innerHeight;
+const particlesArray = [];
+let hue = 0 ;
 
 
-// ===================== start =======================
-// animation start after 1000 miliseconds
-setTimeout(init, 1000);
+window.addEventListener('resize', function(){
+    canvans.width = window.innerWidth;
+    canvans.height = window.innerHeight;
+});
 
-var odrag = document.getElementById('drag-container');
-var ospin = document.getElementById('spin-container');
-var aImg = ospin.getElementsByTagName('img');
-var aVid = ospin.getElementsByTagName('video');
-var aEle = [...aImg, ...aVid]; // combine 2 arrays
-
-// Size of images
-ospin.style.width = imgWidth + "px";
-ospin.style.height = imgHeight + "px";
-
-// Size of ground - depend on radius
-var ground = document.getElementById('ground');
-ground.style.width = radius * 3 + "px";
-ground.style.height = radius * 3 + "px";
-
-function init(delayTime) {
-  for (var i = 0; i < aEle.length; i++) {
-    aEle[i].style.transform = "rotateY(" + (i * (360 / aEle.length)) + "deg) translateZ(" + radius + "px)";
-    aEle[i].style.transition = "transform 1s";
-    aEle[i].style.transitionDelay = delayTime || (aEle.length - i) / 4 + "s";
-  }
+const mouse = {
+    x: undefined,
+    y: undefined ,
 }
 
-function applyTranform(obj) {
-  // Constrain the angle of camera (between 0 and 180)
-  if(tY > 180) tY = 180;
-  if(tY < 0) tY = 0;
+canvans.addEventListener('click', function(e){
+    mouse.x = e.x;
+    mouse.y = e.y;
+    for ( let i =0; i< 10; i++){
+        particlesArray.push(new Particle());
+    }
+});
 
-  // Apply the angle
-  obj.style.transform = "rotateX(" + (-tY) + "deg) rotateY(" + (tX) + "deg)";
+canvans.addEventListener('mousemove', function(e){
+    mouse.x= e.x;
+    mouse.y= e.y;
+    for ( let i =0; i< 5; i++){
+        particlesArray.push(new Particle());
+    }
+})
+
+class Particle {
+    constructor (){
+        this.x = mouse.x;
+        this.y = mouse.y;
+        // this.x = Math.random() * canvans.width;
+        // this.y = Math.random() * canvans.height;
+        this.size = Math.random() * 15 + 1 ;
+        this.speedX = Math.random() * 3 - 1.5 ;
+        this.speedY = Math.random() * 3 - 1.5 ;   
+        this.color = 'hsl('+ hue +', 100%, 50%)';
+     }
+     update(){
+        this.x += this.speedX;
+        this.y += this.speedY;
+        if ( this.size > 0.2) this.size -= 0.1;
+     }
+     draw(){
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.x,  this.y, this.size, 0, Math.PI * 2)
+        ctx.fill();
+    }
 }
 
-function playSpin(yes) {
-  ospin.style.animationPlayState = (yes?'running':'paused');
+
+
+function handleParticles(){
+    for (let i=0 ; i < particlesArray.length ; i++){
+        particlesArray[i].update();
+        particlesArray[i].draw();
+        for(let j = i; j < particlesArray.length ; j++) {
+            const dx = particlesArray[i].x - particlesArray[j].x ;
+            const dy = particlesArray[i].y - particlesArray[j].y ;
+            const distance = Math.sqrt(dx*dx + dy*dy);
+            if (distance <100){
+                ctx.beginPath();
+                ctx.strokeStyle = particlesArray[i].color;
+                ctx.lineWith = 0.2;
+                ctx.moveTo(particlesArray[i].x, particlesArray[i].y);
+                ctx.lineTo(particlesArray[j].x, particlesArray[j].y);
+                ctx.stroke();
+                ctx.closePath();
+            }
+        }   
+        if (particlesArray[i].size <=0.3) {
+            particlesArray.splice( i, 1);
+            i--;
+        }
+     }   
 }
 
-var sX, sY, nX, nY, desX = 0,
-    desY = 0,
-    tX = 0,
-    tY = 10;
-
-// auto spin
-if (autoRotate) {
-  var animationName = (rotateSpeed > 0 ? 'spin' : 'spinRevert');
-  ospin.style.animation = `${animationName} ${Math.abs(rotateSpeed)}s infinite linear`;
+function animate(){
+    ctx.clearRect(0, 0, canvans.width, canvans.height);
+    // ctx.fillStyle = 'rgba(0, 0, 0, 0.02)'
+    // ctx.fillRect(0, 0, canvans.width, canvans.height);
+    handleParticles();
+    hue+=2;
+    requestAnimationFrame(animate);    
 }
-
-
-// setup events
-document.onpointerdown = function (e) {
-  clearInterval(odrag.timer);
-  e = e || window.event;
-  var sX = e.clientX,
-      sY = e.clientY;
-
-  this.onpointermove = function (e) {
-    e = e || window.event;
-    var nX = e.clientX,
-        nY = e.clientY;
-    desX = nX - sX;
-    desY = nY - sY;
-    tX += desX * 0.1;
-    tY += desY * 0.1;
-    applyTranform(odrag);
-    sX = nX;
-    sY = nY;
-  };
-
-  this.onpointerup = function (e) {
-    odrag.timer = setInterval(function () {
-      desX *= 0.95;
-      desY *= 0.95;
-      tX += desX * 0.1;
-      tY += desY * 0.1;
-      applyTranform(odrag);
-      playSpin(false);
-      if (Math.abs(desX) < 0.5 && Math.abs(desY) < 0.5) {
-        clearInterval(odrag.timer);
-        playSpin(true);
-      }
-    }, 17);
-    this.onpointermove = this.onpointerup = null;
-  };
-
-  return false;
-};
-
-document.onmousewheel = function(e) {
-  e = e || window.event;
-  var d = e.wheelDelta / 20 || -e.detail;
-  radius += d;
-  init(1);
-};
+animate()
